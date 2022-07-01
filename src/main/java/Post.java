849cc3fa5685450bd3b0e1a4e9a024e0ccfde9df
VKartsev <vach-partner@mail.ru>
2022-07-01 18:12:18 +0300

    Testing the personal data entry form.

---
+++ src/main/java/Post.java
@@ -0,0 +1,23 @@
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public int birthday;
+
+    public class FormDate {
+
+        public String postBirthday;
+
+        int day = 0;
+        int month = 0;
+        int year = 0;
+
+    }
+}
+
+
+
