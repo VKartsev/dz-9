7027a17cfa7d7a2df8a95dee21b24201782f2d6f
VKartsev <vach-partner@mail.ru>
2022-07-05 22:53:00 +0300

    Testing the personal data entry form.

--- src/main/java/ru/netology/statistic/Post.java
+++ src/main/java/ru/netology/statistic/Post.java
@@ -1,10 +1,16 @@
-package FormDate;
-
+package ru.netology.statistic;
 public class Post {
+
     public String name;
     public String passport;
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
+    public Boolean subscription;
+    public FormDate birthday;
+
+
 }
+
+
+
